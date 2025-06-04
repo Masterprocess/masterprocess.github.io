d332f11645c983f02006a2cba80a6bad6f49db8c
Zoe Rackley <misterrackley@gmail.com>
2025-06-04 15:55:37 -0500

    Add Tailwind CSS support

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./gatsby-ssr.js",
+    "./gatsby-browser.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
