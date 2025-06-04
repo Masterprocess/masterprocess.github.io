c8d77e9661357f8d2b68d97ee1f4cc668178ac62
Zoe Rackley <misterrackley@gmail.com>
2025-06-04 00:43:21 -0500

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Zoe Rackley — UX Design`,
+    description: `Portfolio of Bauhaus-inspired user experience projects and design explorations.`,
     author: `@gatsbyjs`,
     siteUrl: `https://masterprocess.github.io`, // Updated to reflect my GitHub Pages URL
   },
