a881b5b9202cbe3bb03347f66ddaf4eb46cacb2c
Zoe Rackley <misterrackley@gmail.com>
2025-06-04 16:03:02 -0500

    Add Header and Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-bauhausBlack py-4 text-center text-white">
+    © {new Date().getFullYear()} Zoe Rackley
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const Header = () => (
+  <header className="bg-bauhausRed text-white">
+    <div className="container mx-auto flex flex-wrap items-center justify-between px-4 py-4">
+      <Link to="/" className="font-bold hover:underline">
+        Zoe Rackley
+      </Link>
+      <nav className="flex flex-wrap space-x-4">
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/contact" className="hover:underline">Contact</Link>
+        <Link to="/resume" className="hover:underline">Resume</Link>
+      </nav>
+    </div>
+  </header>
+);
+
+export default Header;
