c86092aa6fb767b0b9a210fb6d14f341123ea335
Zoe Rackley <misterrackley@gmail.com>
2025-06-04 16:37:05 -0500

    Import Tailwind globals in Gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,7 @@
+import "./src/styles/global.css";
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
  */
-
-import "./src/styles/global.css";
-
 module.exports = {};
