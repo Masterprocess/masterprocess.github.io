1d067103e5b1710a6245f8a83712b149dceefa4a
Zoe Rackley <misterrackley@gmail.com>
2025-06-04 22:48:07 -0500

    Add padding to blog pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ children, showFooter = true }: LayoutProps) => {
   return (
     <div className="flex min-h-screen flex-col font-sans">
       <Header />
-      <main className="flex-grow pt-20">{children}</main>
+      <main className="flex-grow">{children}</main>
       {showFooter && <Footer />}
     </div>
   );
