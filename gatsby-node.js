339c8ad6d5a8c1f1871661bca405827a79853e13
Zoe Rackley <misterrackley@gmail.com>
2025-06-04 21:23:26 -0500

    Fix blog build

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,15 +4,61 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  createTypes(`
+    type Mdx implements Node {
+      fields: MdxFields
+    }
+    type MdxFields {
+      slug: String
+    }
+  `);
+};
+
 /**
- * @type {import('gatsby').GatsbyNode['createPages']}
+ * Add a slug field to MDX nodes based on their file path.
  */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === "Mdx") {
+    const slug = createFilePath({ node, getNode, basePath: "docs" });
+    createNodeField({ node, name: "slug", value: `/blog${slug}` });
+  }
+};
+
+/**
+ * Create a page for each MDX file under docs/.
+ */
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const postTemplate = path.resolve("./src/templates/blog-post.tsx");
+  const result = await graphql(`
+    {
+      allMdx {
+        nodes {
+          id
+          internal {
+            contentFilePath
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) throw result.errors;
+
+  result.data.allMdx.nodes.forEach(({ id, internal, fields: { slug } }) => {
+    createPage({
+      path: slug,
+      component: `${postTemplate}?__contentFilePath=${internal.contentFilePath}`,
+      context: { id },
+    });
+  });
+};
---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Seo from "../components/seo";
+
+export const query = graphql`
+  query BlogPost($id: String!) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "YYYY-MM-DD")
+      }
+    }
+  }
+`;
+
+type BlogPostProps = {
+  data: {
+    mdx: {
+      frontmatter: { title: string; date: string };
+    };
+  };
+};
+
+export default function BlogPost({
+  data,
+  children,
+}: React.PropsWithChildren<BlogPostProps>) {
+  const { frontmatter } = data.mdx;
+  return (
+    <Layout>
+      <article className="prose md:prose-lg mx-auto dark:prose-invert">
+        <h1>{frontmatter.title}</h1>
+        <p className="text-sm opacity-70">{frontmatter.date}</p>
+        {children}
+      </article>
+    </Layout>
+  );
+}
+
+export const Head = ({ data }: BlogPostProps) => (
+  <Seo title={data.mdx.frontmatter.title} />
+);
