fbd0bbe59a07d246ff5ae228ac7044bb81d18dcb
Zoe Rackley <chthoniczoe@gmail.com>
2026-01-27 16:59:00 -0600

    Reset

--- gatsby-browser.ts
+++
@@ -1,8 +0,0 @@
-import './src/styles/global.css';
-import './src/styles/design-system.scss';
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
-export {};
